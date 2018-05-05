8e74190f25df18352c7a99bf0b6cd2518f2bb6cd
legalunlun <hhpsdhh@gmail.com>
2018-05-05 15:55:14 +0900

    add MyLocation by Legal

---
+++ gazer/src/MyLocation.swift
@@ -0,0 +1,41 @@
+//
+//  GetLocation.swift
+//  gazer
+//
+//  Created by LeGal on 2018/05/05.
+//  Copyright © 2018年 OCTA. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class MyLocation: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager!
+    
+    override func GetLocation() {
+        super.GetLocation()
+        setupLocationManager()
+    }
+    
+    func setupLocationManager() {
+        locationManager = CLLocationManager()
+        guard let locationManager = locationManager else { return }
+        locationManager.requestWhenInUseAuthorization()
+        
+        let status = CLLocationManager.authorizationStatus()
+        if status == .authorizedWhenInUse {
+            locationManager.delegate = self
+            locationManager.distanceFilter = 10
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first
+        let latitude = location?.coordinate.latitude
+        let longitude = location?.coordinate.longitude
+        
+        print("latitude: \(latitude!)\nlongitude: \(longitude!)")
+    }
+}

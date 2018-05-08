04bf5056c0137f039a27848e35681aa6fd63b6bd
legalunlun <hhpsdhh@gmail.com>
2018-05-08 10:42:27 +0900

    add Location

--- gazer/ViewController.swift
+++ gazer/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, CLLocationManagerDele
         super.viewDidLoad()
         
 //        MyLocation().getLocation()
+//
         
       
         sceneView.delegate = self

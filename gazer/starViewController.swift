953d71558c529b37b2dbdeed0468058dff9907a7
keisuke.k <kesk.k99@gmail.com>
2018-06-18 11:39:10 +0900

    test

--- gazer/starViewController.swift
+++ gazer/starViewController.swift
@@ -74,14 +74,14 @@ class starViewController: UIViewController, ARSCNViewDelegate, CLLocationManager
         let rightAscension: Double  // 赤経
         let declination: Double     // 赤緯
         let magnitude: Double       // 等級
-        let details: String          // 詳細説明
+        let details: String         // 詳細説明
     }
     
     // 星のデータ配列
     let stars: [Star] = [
-        (Star(hipId: 32349, enName: "Sirius", jpName: "シリウス", rightAscension: 6.45925, declination: -16.42473, magnitude: -1.44, details: "詳細説明")),
         (Star(hipId: 27989, enName: "Betelgeuse", jpName: "ベテルギウス", rightAscension: 5.551029, declination: 7.24253, magnitude: 0.58, details: "詳細説明")),
-        (Star(hipId: 37279, enName: "Procyon", jpName: "プロキオン", rightAscension: 7.391854, declination: 5.1339, magnitude: 0.34, details: "詳細説明"))
+        (Star(hipId: 37279, enName: "Procyon", jpName: "プロキオン", rightAscension: 7.391854, declination: 5.1339, magnitude: 0.34, details: "詳細説明")),
+        (Star(hipId: 32349, enName: "Sirius", jpName: "シリウス", rightAscension: 6.45925, declination: -16.42473, magnitude: -1.44, details: "詳細説明"))
     ]
         
     override func viewDidLoad() {

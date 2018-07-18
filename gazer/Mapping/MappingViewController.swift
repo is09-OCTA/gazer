424ebb110d9b34475d68eb4ac58c3866bd1fc7ae
dorudorujr <a1209s518n928@gmail.com>
2018-07-18 10:29:56 +0900

    プロジェクションマッピング 実装

--- gazer/Mapping/MappingViewController.swift
+++ gazer/Mapping/MappingViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import ARKit
 import SceneKit
+import AVFoundation
 
 class MappingViewController: UIViewController, ARSCNViewDelegate {
   
@@ -18,12 +19,24 @@ class MappingViewController: UIViewController, ARSCNViewDelegate {
     super.viewDidLoad()
     
     sceneView.delegate = self
+    // Show statistics such as fps and timing information
+    sceneView.showsStatistics = true
+    
+    // Create a new scene
+    let scene = SCNScene()
+    //sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+    
+    // Set the scene to the view
+    sceneView.scene = scene
   }
   
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     
     let configuration = ARWorldTrackingConfiguration()
+    configuration.planeDetection = [.vertical, .horizontal]
+    //画像認識を可能に設定
+    configuration.detectionImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil)
     
     sceneView.session.run(configuration)
   }
@@ -37,6 +50,72 @@ class MappingViewController: UIViewController, ARSCNViewDelegate {
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
   }
+  
+  
+  func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+    guard let cuptureImage = sceneView.session.currentFrame?.capturedImage else {
+      return
+    }
+    
+    // PixelBuffer を CIImage に変換しフィルターをかける
+    let ciImage = CIImage.init(cvPixelBuffer: cuptureImage)
+    let filter:CIFilter = CIFilter(name: "CIColorMonochrome")!
+    filter.setValue(ciImage, forKey: kCIInputImageKey)
+    filter.setValue(CIColor(red: 0.75, green: 0.75, blue: 0.75), forKey: "inputColor")
+    filter.setValue(1.0, forKey: "inputIntensity")
+    
+    //　CIImage を CGImage に変換して背景に適応
+    //　カメラ画像はホーム右のランドスケープの状態で画像が渡されるため、CGImagePropertyOrientation(rawValue: 6) でポートレートで正しい向きに表示されるよう変換
+    let context = CIContext()
+    let result = filter.outputImage!.oriented(CGImagePropertyOrientation(rawValue: 6)!)
+    if let cgImage = context.createCGImage(result, from: result.extent) {
+      sceneView.scene.background.contents = cgImage
+    }
+  }
+  
+  func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+    DispatchQueue.main.async {
+      if let imageAnchor = anchor as? ARImageAnchor{
+        if(node.geometry == nil){
+          let plane = SCNPlane()
+          plane.width = imageAnchor.referenceImage.physicalSize.width
+          plane.height = (imageAnchor.referenceImage.physicalSize.height) * 2
+          node.geometry = plane
+        }
+        
+        node.simdTransform = imageAnchor.transform
+        node.geometry?.firstMaterial = self.createMaterial()
+        node.eulerAngles.x = 0
+        //原点の移動
+        node.pivot = SCNMatrix4MakeTranslation(0.0, -0.5, 0.0)
+      }
+    }
+  }
+  
+  func createMaterial() -> SCNMaterial {
+    // ビデオのURL
+    let videoUrl = Bundle.main.url(forResource: "default", withExtension: "mp4")!
+    // AVPlayerを生成する
+    let avPlayer = AVPlayer(url: videoUrl)
+    // SKSceneを生成する
+    let skScene = SKScene(size: CGSize(width: 1000, height: 1000))
+    // AVPlayerからSKVideoNodeの生成する
+    let skNode = SKVideoNode(avPlayer: avPlayer)
+    // シーンと同じサイズとし、中央に配置する
+    skNode.position = CGPoint(x: skScene.size.width / 2.0, y: skScene.size.height / 2.0)
+    skNode.size = skScene.size
+    skNode.yScale = -1.0 // 座標系を上下逆にする
+    skNode.play() // 再生開始
+    // SKSceneに、SKVideoNodeを追加する
+    skScene.addChild(skNode)
+    
+    let material = SCNMaterial()
+    material.diffuse.contents = skScene
+    material.transparency = 0.7
+    return material
+  }
+  
+  
   
   func session(_ session: ARSession, didFailWithError error: Error) {
   }

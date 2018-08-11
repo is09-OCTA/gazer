cdbfb4eb684d44a8e62cdb12332dad854e5a877c
dorudorujr <a1209s518n928@gmail.com>
2018-08-11 12:53:32 +0900

    画像認識の画像を変更し、微調整

--- gazer/Mapping/MappingViewController.swift
+++ gazer/Mapping/MappingViewController.swift
@@ -36,7 +36,12 @@ class MappingViewController: UIViewController, ARSCNViewDelegate, AVAudioPlayerD
     let configuration = ARWorldTrackingConfiguration()
     configuration.planeDetection = [.vertical, .horizontal]
     //画像認識を可能に設定
-    configuration.detectionImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil)
+    guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
+      fatalError("Missing expected asset catalog resources.")
+    }
+    configuration.detectionImages = referenceImages
+    
+    
     
     sceneView.session.run(configuration)
   }
@@ -73,7 +78,7 @@ class MappingViewController: UIViewController, ARSCNViewDelegate, AVAudioPlayerD
     }
   }
   // 画像認識
-  func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+  /*func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
     DispatchQueue.main.async {
       if let imageAnchor = anchor as? ARImageAnchor{
         if(node.geometry == nil){
@@ -82,7 +87,7 @@ class MappingViewController: UIViewController, ARSCNViewDelegate, AVAudioPlayerD
           let videoVideoUrl = Bundle.main.url(forResource: "main", withExtension: "mp4")!
           let rockVideoUrl = Bundle.main.url(forResource: "RockMaterial2", withExtension: "mp4")!
           plane.width = imageAnchor.referenceImage.physicalSize.width
-          plane.height = (imageAnchor.referenceImage.physicalSize.height) * 1.8
+          plane.height = (imageAnchor.referenceImage.physicalSize.height) * 3.0
           let videoNode = SCNNode()
           videoNode.geometry = plane
           let videoMaterial = self.createMaterial(videoUrl: videoVideoUrl, alpha: 0.7,angle: 0.0)
@@ -95,19 +100,84 @@ class MappingViewController: UIViewController, ARSCNViewDelegate, AVAudioPlayerD
           node.addChildNode(rockNode as SCNNode)
           
         }
+        //ライト
+        /*let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light?.type = .ambient
+        lightNode.position = SCNVector3(x:0, y:10, z:1)
+        node.addChildNode(lightNode as SCNNode)*/
+        node.simdTransform = imageAnchor.transform
+        node.eulerAngles.x = 0
+        // 原点の移動
+        node.pivot = SCNMatrix4MakeTranslation(0.0, 1.0, 0.0)
+        self.playSound(name: "sound")
+      }
+    }
+  }*/
+  
+  
+  /*func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+    DispatchQueue.main.async {
+      guard let imageAnchor = anchor as? ARImageAnchor else {
+        print("Error: This anchor is not ARPlaneAnchor. [\(#function)]")
+        return
+      }
+      
+      guard let geometryPlaneNode = node.childNodes.first,
+        let plane = geometryPlaneNode.geometry as? SCNPlane else {
+          print("Error: SCNPlane node is not found. [\(#function)]")
+          return
+      }
+      plane.width = imageAnchor.referenceImage.physicalSize.width
+      plane.height = (imageAnchor.referenceImage.physicalSize.height) * 1.8
+      node.simdTransform = imageAnchor.transform
+      
+      
+    }
+  }*/
+  
+  func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+    DispatchQueue.main.async {
+      if let imageAnchor = anchor as? ARImageAnchor {
+        if(node.geometry == nil){
+          let plane = SCNPlane()
+          // ビデオのURL
+          let videoVideoUrl = Bundle.main.url(forResource: "main", withExtension: "mp4")!
+          let rockVideoUrl = Bundle.main.url(forResource: "RockMaterial2", withExtension: "mp4")!
+          plane.width = imageAnchor.referenceImage.physicalSize.width
+          plane.height = (imageAnchor.referenceImage.physicalSize.height) * 3.0
+          let videoNode = SCNNode()
+          videoNode.geometry = plane
+          let videoMaterial = self.createMaterial(videoUrl: videoVideoUrl, alpha: 0.7,angle: 0.0)
+          videoNode.geometry?.firstMaterial = videoMaterial
+          node.addChildNode(videoNode as SCNNode)
+          
+          let rockNode = MappingViewController.collada2SCNNode(filepath: "art.scnassets/Mossy Rock - stump.scn")
+          let rockMaterial = self.createMaterial(videoUrl: rockVideoUrl, alpha: 1.0,angle: 90.0)
+          rockNode.childNodes[0].geometry?.firstMaterial = rockMaterial
+          node.addChildNode(rockNode as SCNNode)
+          
+        }
+        //ライト
         let lightNode = SCNNode()
         lightNode.light = SCNLight()
         lightNode.light?.type = .ambient
         lightNode.position = SCNVector3(x:0, y:10, z:1)
+        
+        
         node.addChildNode(lightNode as SCNNode)
         node.simdTransform = imageAnchor.transform
-        node.eulerAngles.x = 0
+        //座標を合わせる
+        //node.eulerAngles.x = -.pi / 2
+        //node.eulerAngles.x = 0
+        node.rotation = SCNVector4(0, 1, 0, 0.25 * Double.pi)
         // 原点の移動
-        node.pivot = SCNMatrix4MakeTranslation(0.0, -0.5, 0.0)
+        node.pivot = SCNMatrix4MakeTranslation(0.0, 1.0, 0.0)
         self.playSound(name: "sound")
       }
     }
   }
+  
   // オブジェクト用の動画 materialを生成
   func createMaterial(videoUrl: URL,alpha: Float,angle: Float) -> SCNMaterial {
     // AVPlayerを生成する

5466e488b1c2b0cbc7c93ed866e5be678cd55a3c
keisuke.k <kesk.k99@gmail.com>
2018-09-22 11:51:27 +0900

    add ZooModalViewComtroller

---
+++ gazer/Zoo/ZooModalViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ZooModalViewController.swift
+//  
+//
+//  Created by Keisuke Kitamura on 2018/09/22.
+//
+
+import UIKit
+
+class ZooModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

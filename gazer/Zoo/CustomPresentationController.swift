af88969f33cbe9d457ea685c96d01c52a0074ae7
keisuke.k <kesk.k99@gmail.com>
2018-09-28 14:12:18 +0900

    update

--- gazer/Zoo/CustomPresentationController.swift
+++ gazer/Zoo/CustomPresentationController.swift
@@ -43,7 +43,7 @@ class CustomPresentationController: UIPresentationController {
         }
     }
     
-    let margin = (x: CGFloat(0), y: CGFloat(UIScreen.main.bounds.size.height / 2.8))
+    let margin = (x: CGFloat(0), y: CGFloat(UIScreen.main.bounds.size.height * (3.5 / 10)))
     // 子のコンテナサイズを返す
     override func size(forChildContentContainer container: UIContentContainer, withParentContainerSize parentSize: CGSize) -> CGSize {
         return CGSize(width: parentSize.width - margin.x, height: parentSize.height - margin.y)
--- gazer/Zoo/ZooModalViewController.swift
+++ gazer/Zoo/ZooModalViewController.swift
@@ -13,21 +13,20 @@ class ZooModalViewController: UIViewController, UIScrollViewDelegate {
 
     private var pageControl: UIPageControl!
     private var scrollView: UIScrollView!
-
-    let zebraImage:UIImage = UIImage(named:"ZooImage/Zebra")!
-    let unicornImage:UIImage = UIImage(named:"ZooImage/Unicorn")!
-
-    
-    // ボタン配置変数
-    var prevButton:CGFloat = 20
-    
-    // ボタンの横幅、縦幅設定定数
-    let buttonWidth:CGFloat = 180
-    let buttonHeight:CGFloat = 180
     
     // 画面の横幅、縦幅
     let width:CGFloat = UIScreen.main.bounds.size.width
     let height:CGFloat = UIScreen.main.bounds.size.height
+    
+    // ボタンの画像
+    let zebraImage:UIImage = UIImage(named:"ZooImage/Zebra")!
+    let unicornImage:UIImage = UIImage(named:"ZooImage/Unicorn")!
+    
+    // ボタン配置変数
+    var prevButton:CGFloat = 20
+    
+    // ボタンの辺の長さ
+    let buttonSide:CGFloat = (UIScreen.main.bounds.size.width * 12) / 30
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,154 +52,46 @@ class ZooModalViewController: UIViewController, UIScrollViewDelegate {
             // フォントサイズ
             label.font = UIFont.systemFont(ofSize: 36)
             
-            
             if i == 0{
 
-                //Zebra
-                //label.text = "Zebra"
-                //scrollView.addSubview(label)
-                // 影表示用のビュー
-                let zebraShadowView = UIView(frame: CGRect(x: 0, y: 0, width: buttonWidth, height: buttonHeight ))
-                prevButton = buttonHeight / 2 + 250
-                zebraShadowView.center = CGPoint(x: width  / 2 - 95, y: prevButton - 240)
-                zebraShadowView.layer.shadowColor = UIColor.black.cgColor
-                zebraShadowView.layer.shadowOpacity = 0.5
-                zebraShadowView.layer.shadowOffset = CGSize(width: 5, height: 5)
-                zebraShadowView.layer.shadowRadius = 5
-                // 画像ボタン
-                let zebraButton = UIButton(frame: zebraShadowView.bounds)
+                // 左上
+                let zebraButton = UIButton()
                 zebraButton.setBackgroundImage(zebraImage, for: [])
-                zebraButton.layer.cornerRadius = 12
-                zebraButton.layer.masksToBounds = true
-                // 影表示用のビューに画像ボタンを乗せる
-                zebraShadowView.addSubview(zebraButton)
-                // 影表示+画像ボタンのビューを乗せる
-                scrollView.addSubview(zebraShadowView)
-
-                //Unicorn
-                //label.text = "Unicorn"
-                //scrollView.addSubview(label)
-                // 影表示用のビュー
-                let unicornShadowView = UIView(frame: CGRect(x: 0, y: 0, width: buttonWidth, height: buttonHeight ))
-                prevButton = buttonHeight / 2 + 250
-                unicornShadowView.center = CGPoint(x: width  / 2 + 95, y: prevButton - 240)
-                unicornShadowView.layer.shadowColor = UIColor.black.cgColor
-                unicornShadowView.layer.shadowOpacity = 0.5
-                unicornShadowView.layer.shadowOffset = CGSize(width: 5, height: 5)
-                unicornShadowView.layer.shadowRadius = 5
-                // 画像ボタン
-                let unicornButton = UIButton(frame: unicornShadowView.bounds)
+                zebraButton.layer.cornerRadius = 5
+                zebraButton.layer.masksToBounds  = true
+                zebraButton.frame = CGRect(x: (width / 30) * 2, y: (width / 30) * 4,
+                                           width: buttonSide, height: buttonSide)
+                scrollView.addSubview(zebraButton)
+                
+                // 右上
+                let unicornButton = UIButton()
                 unicornButton.setBackgroundImage(unicornImage, for: [])
-                unicornButton.layer.cornerRadius = 12
-                unicornButton.layer.masksToBounds = true
-                // 影表示用のビューに画像ボタンを乗せる
-                unicornShadowView.addSubview(unicornButton)
-                // 影表示+画像ボタンのビューを乗せる
-                scrollView.addSubview(unicornShadowView)
+                unicornButton.layer.cornerRadius = 5
+                unicornButton.layer.masksToBounds  = true
+                unicornButton.frame = CGRect(x: (width / 30) * 4 + buttonSide, y: (width / 30) * 4,
+                                           width: buttonSide, height: buttonSide)
+                scrollView.addSubview(unicornButton)
                 
+                // 左下
+                let sheepButton = UIButton()
+                sheepButton.setBackgroundImage(unicornImage, for: [])
+                sheepButton.layer.cornerRadius = 5
+                sheepButton.layer.masksToBounds  = true
+                sheepButton.frame = CGRect(x: (width / 30) * 2, y: (width / 30) * 6 + buttonSide,
+                                             width: buttonSide, height: buttonSide)
+                scrollView.addSubview(sheepButton)
+
+                // 右下
+                let hippopotamusButton = UIButton()
+                hippopotamusButton.setBackgroundImage(zebraImage, for: [])
+                hippopotamusButton.layer.cornerRadius = 5
+                hippopotamusButton.layer.masksToBounds  = true
+                hippopotamusButton.frame = CGRect(x: (width / 30) * 4 + buttonSide, y: (width / 30) * 6 + buttonSide,
+                                           width: buttonSide, height: buttonSide)
+                scrollView.addSubview(hippopotamusButton)
                 
-                //Sheep
-                //label.text = "Sheep"
-                //scrollView.addSubview(label)
-                // 影表示用のビュー
-                let sheepShadowView = UIView(frame: CGRect(x: 0, y: 0, width: buttonWidth, height: buttonHeight ))
-                prevButton = buttonHeight / 2 + 250
-                sheepShadowView.center = CGPoint(x: width  / 2 - 95, y: prevButton - 55)
-                sheepShadowView.layer.shadowColor = UIColor.black.cgColor
-                sheepShadowView.layer.shadowOpacity = 0.5
-                sheepShadowView.layer.shadowOffset = CGSize(width: 5, height: 5)
-                sheepShadowView.layer.shadowRadius = 5
-                // 画像ボタン
-                let sheepButton = UIButton(frame: sheepShadowView.bounds)
-                sheepButton.setBackgroundImage(zebraImage, for: [])
-                sheepButton.layer.cornerRadius = 12
-                sheepButton.layer.masksToBounds = true
-                // 影表示用のビューに画像ボタンを乗せる
-                sheepShadowView.addSubview(sheepButton)
-                // 影表示+画像ボタンのビューを乗せる
-                scrollView.addSubview(sheepShadowView)
+            } else if i == 1 {
                 
-                
-                //Hippopotamus
-                //label.text = "Hippopotamus"
-                //scrollView.addSubview(label)
-                // 影表示用のビュー
-                let HippopotamusShadowView = UIView(frame: CGRect(x: 0, y: 0, width: buttonWidth, height: buttonHeight ))
-                prevButton = buttonHeight / 2 + 250
-                HippopotamusShadowView.center = CGPoint(x: width  / 2 + 95, y: prevButton - 55)
-                HippopotamusShadowView.layer.shadowColor = UIColor.black.cgColor
-                HippopotamusShadowView.layer.shadowOpacity = 0.5
-                HippopotamusShadowView.layer.shadowOffset = CGSize(width: 5, height: 5)
-                HippopotamusShadowView.layer.shadowRadius = 5
-                // 画像ボタン
-                let HippopotamusButton = UIButton(frame: HippopotamusShadowView.bounds)
-                HippopotamusButton.setBackgroundImage(zebraImage, for: [])
-                HippopotamusButton.layer.cornerRadius = 12
-                HippopotamusButton.layer.masksToBounds = true
-                // 影表示用のビューに画像ボタンを乗せる
-                HippopotamusShadowView.addSubview(HippopotamusButton)
-                // 影表示+画像ボタンのビューを乗せる
-                scrollView.addSubview(HippopotamusShadowView)
-/*
-                let button1 = UIButton()
-                button1.frame = CGRect(x:0, y:0,
-                                      width:buttonWidth, height:buttonHeight)
-                prevButton = buttonHeight / 2 + 250
-                button1.center = CGPoint(x: width  / 2 - 95, y: prevButton - 240)
-                button1.setBackgroundImage(zebraImage, for: [])
-                scrollView.addSubview(button1)
-                
-                let button2 = UIButton()
-                button2.frame = CGRect(x:0, y:0,
-                                       width:buttonWidth, height:buttonHeight)
-                prevButton = buttonHeight / 2 + 250
-                button2.center = CGPoint(x: width  / 2 + 95, y: prevButton - 240)
-                button2.setBackgroundImage(zebraImage, for: [])
-                scrollView.addSubview(button2)
-                
-                let button3 = UIButton()
-                button3.frame = CGRect(x:0, y:0,
-                                       width:buttonWidth, height:buttonHeight)
-                prevButton = buttonHeight / 2 + 250
-                button3.center = CGPoint(x: width  / 2 - 95, y: prevButton - 50)
-                button3.setBackgroundImage(zebraImage, for: [])
-                scrollView.addSubview(button3)
-                
-                let button4 = UIButton()
-                button4.frame = CGRect(x:0, y:0,
-                                       width:buttonWidth, height:buttonHeight)
-                prevButton = buttonHeight / 2 + 250
-                button4.center = CGPoint(x: width  / 2 + 95, y: prevButton - 50)
-                button4.setBackgroundImage(zebraImage, for: [])
-                scrollView.addSubview(button4)
-                
-*/
-                
-                
-                
-            }else if i == 1{
-/*
-                //Zebra
-                //label.text = "Zebra"
-                //scrollView.addSubview(label)
-                // 影表示用のビュー
-                let zebraShadowView = UIView(frame: CGRect(x: 0, y: 0, width: buttonWidth, height: buttonHeight ))
-                prevButton = buttonHeight / 2 + 250
-                zebraShadowView.center = CGPoint(x: width  / 2 - 100, y: prevButton - 150)
-                zebraShadowView.layer.shadowColor = UIColor.black.cgColor
-                zebraShadowView.layer.shadowOpacity = 0.5
-                zebraShadowView.layer.shadowOffset = CGSize(width: 5, height: 5)
-                zebraShadowView.layer.shadowRadius = 5
-                // 画像ボタン
-                let zebraButton = UIButton(frame: zebraShadowView.bounds)
-                zebraButton.setBackgroundImage(gorillaGifImage, for: [])
-                zebraButton.layer.cornerRadius = 12
-                zebraButton.layer.masksToBounds = true
-                // 影表示用のビューに画像ボタンを乗せる
-                zebraShadowView.addSubview(zebraButton)
-                // 影表示+画像ボタンのビューを乗せる
-                scrollView.addSubview(zebraShadowView)
-*/
             }
         }
         
@@ -208,7 +99,7 @@ class ZooModalViewController: UIViewController, UIScrollViewDelegate {
         pageControl = UIPageControl()
         
         //pageControlの位置とサイズを設定
-        pageControl.frame = CGRect(x:0, y:height - 170, width:width, height:50)
+        pageControl.frame = CGRect(x: 0, y: height / 1.7, width: width, height: 50)
         
         // インジケータの色合い
         pageControl.currentPageIndicatorTintColor = UIColor.init(red: 56/255, green: 151/255, blue: 240/255, alpha: 1.0)

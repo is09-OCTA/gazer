4d73f67e05113ea4ed072b828573649ac69d2654
Syamoti <shot.sauser@gmail.com>
2018-10-24 11:26:16 +0900

    水族館　ウォークスルー実装

--- gazer/Aquarium/AquariumViewController.swift
+++ gazer/Aquarium/AquariumViewController.swift
@@ -11,8 +11,9 @@ import ARKit
 import SceneKit
 import AVFoundation
 import SCLAlertView
+import EAIntroView
 
-class AquariumViewController: UIViewController, ARSCNViewDelegate, AVAudioPlayerDelegate{
+class AquariumViewController: UIViewController, ARSCNViewDelegate, AVAudioPlayerDelegate, EAIntroDelegate{
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -41,6 +42,8 @@ class AquariumViewController: UIViewController, ARSCNViewDelegate, AVAudioPlayer
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        aquariumIntroView()
         
         sceneView.delegate = self
         
@@ -103,6 +106,52 @@ class AquariumViewController: UIViewController, ARSCNViewDelegate, AVAudioPlayer
             node.addChildNode(childNode as SCNNode)
         }
         return node
+    }
+    
+    // EAIntroView
+    func aquariumIntroView(){
+        // １ページ目
+        let firstIntro = EAIntroPage()
+        firstIntro.title = "ようこそ！"
+        firstIntro.desc = """
+        画面を注視しながらの歩行は大変危険です。
+        画面をタップすると次のページに移ります。
+        """
+        firstIntro.descPositionY = self.view.bounds.size.height/1.5
+        firstIntro.bgColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.7)
+        
+        // ２ページ目
+        let secondIntro = EAIntroPage()
+        secondIntro.title = "メニューに戻りたいときは？"
+        secondIntro.desc = """
+        右にスワイプすると戻れます
+        """
+        secondIntro.bgImage = UIImage(named:"introSecond")
+        
+        // 3ページ目
+        let thirdIntro = EAIntroPage()
+        thirdIntro.title = "水槽をだすには？"
+        thirdIntro.desc = """
+        壁に向かってタップするとさ水槽が出現します
+        """
+        thirdIntro.bgImage = UIImage(named: "introThird")
+        
+        // フォント設定
+        firstIntro.titleFont = UIFont(name: "HelveticaNeue-Bold", size: 45)
+        firstIntro.descFont = UIFont(name:"HelveticaNeue-Light",size:15)
+        secondIntro.descFont = UIFont(name:"HelveticaNeue-Light",size:15)
+        secondIntro.titleFont = UIFont(name: "HelveticaNeue-Bold", size: 20)
+        thirdIntro.descFont = UIFont(name:"HelveticaNeue-Light",size:15)
+        thirdIntro.titleFont = UIFont(name: "HelveticaNeue-Bold", size: 20)
+        
+        let introView = EAIntroView(frame: self.view.bounds, andPages: [firstIntro,secondIntro,thirdIntro])
+        // スキップ
+        introView?.skipButton.setTitle("スキップ", for: UIControl.State.normal)
+        introView?.delegate = self
+        // タップで次へ進む
+        introView?.tapToNext = true
+        // 画面立ち上げ
+        introView?.show(in: self.view, animateDuration: 1.0)
     }
     
     //BGM

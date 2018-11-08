51ef06050444aa16e991bf80286c9d5b0c78f9ca
dorudorujr <a1209s518n928@gmail.com>
2018-11-08 11:33:22 +0900

    Mapping結合

--- gazer/Mapping/MappingViewController.swift
+++ gazer/Mapping/MappingViewController.swift
@@ -12,10 +12,10 @@ import SceneKit
 import AVFoundation
 import SCLAlertView
 
-class MappingViewController: UIViewController, ARSCNViewDelegate, AVAudioPlayerDelegate {
+class MappingViewController: UIViewController, ARSCNViewDelegate {
   
   @IBOutlet var sceneView: ARSCNView!
-  var audioPlayer: AVAudioPlayer!
+  var disneyCastleNode: DisneyCastleNode?
     
     @IBOutlet weak var button: UIButton!
     
@@ -39,29 +39,22 @@ class MappingViewController: UIViewController, ARSCNViewDelegate, AVAudioPlayerD
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    sceneView.delegate = self
-    // Show statistics such as fps and timing information
-    sceneView.showsStatistics = true
+    // Set the view's delegate
+    sceneView.delegate = self as ARSCNViewDelegate
     
     // Create a new scene
     let scene = SCNScene()
     
     // Set the scene to the view
     sceneView.scene = scene
+    self.registerGestureRecognizer()
   }
   
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     
     let configuration = ARWorldTrackingConfiguration()
-    configuration.planeDetection = [.vertical, .horizontal]
-    //画像認識を可能に設定
-    guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
-      fatalError("Missing expected asset catalog resources.")
-    }
-    configuration.detectionImages = referenceImages
-    
-    
+    configuration.planeDetection = .horizontal
     
     sceneView.session.run(configuration)
   }
@@ -72,147 +65,28 @@ class MappingViewController: UIViewController, ARSCNViewDelegate, AVAudioPlayerD
     sceneView.session.pause()
   }
   
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
+  //tap設定
+  private func registerGestureRecognizer() {
+    let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(MappingViewController.tapped))
+    self.sceneView.addGestureRecognizer(tapGestureRecognizer)
   }
   
-  // 背景を暗くする
-  func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
-    guard let cuptureImage = sceneView.session.currentFrame?.capturedImage else {
+  @objc func tapped(sender: UITapGestureRecognizer) {
+    // すでに追加済みであれば無視
+    if self.disneyCastleNode != nil {
       return
     }
-    
-    // PixelBuffer を CIImage に変換しフィルターをかける
-    let ciImage = CIImage.init(cvPixelBuffer: cuptureImage)
-    let filter:CIFilter = CIFilter(name: "CIColorMonochrome")!
-    filter.setValue(ciImage, forKey: kCIInputImageKey)
-    filter.setValue(CIColor(red: 0.2, green: 0.2, blue: 0.2), forKey: "inputColor")
-    filter.setValue(1.0, forKey: "inputIntensity")
-    
-    // CIImage を CGImage に変換して背景に適応
-    // カメラ画像はホーム右のランドスケープの状態で画像が渡されるため、CGImagePropertyOrientation(rawValue: 6) でポートレートで正しい向きに表示されるよう変換
-    let context = CIContext()
-    let result = filter.outputImage!.oriented(CGImagePropertyOrientation(rawValue: 6)!)
-    if let cgImage = context.createCGImage(result, from: result.extent) {
-      sceneView.scene.background.contents = cgImage
-    }
+    self.addItem()
+  }
+  
+  private func addItem() {
+    disneyCastleNode = DisneyCastleNode()
+    sceneView.scene.rootNode.addChildNode(disneyCastleNode!)
   }
   
   
   
-  func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-    DispatchQueue.main.async {
-      guard let imageAnchor = anchor as? ARImageAnchor else {
-        print("Error: This anchor is not ARPlaneAnchor. [\(#function)]")
-        return
-      }
-      
-      guard let geometryPlaneNode = node.childNodes.first,
-        let plane = geometryPlaneNode.geometry as? SCNPlane else {
-          print("Error: SCNPlane node is not found. [\(#function)]")
-          return
-      }
-      plane.width = imageAnchor.referenceImage.physicalSize.width
-      plane.height = (imageAnchor.referenceImage.physicalSize.height) * 2.1
-      node.simdTransform = imageAnchor.transform
-      node.eulerAngles.x = 0
-      // 原点の移動
-      node.pivot = SCNMatrix4MakeTranslation(0.0, 0.7, 0.0)
-    }
-  }
   
-  func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-    DispatchQueue.main.async {
-      if let imageAnchor = anchor as? ARImageAnchor{
-        if(node.geometry == nil){
-          let plane = SCNPlane()
-          // ビデオのURL
-          let videoVideoUrl = Bundle.main.url(forResource: "main5", withExtension: "mp4")!
-          let rockVideoUrl = Bundle.main.url(forResource: "RockMaterial3", withExtension: "mp4")!
-          plane.width = imageAnchor.referenceImage.physicalSize.width
-          plane.height = (imageAnchor.referenceImage.physicalSize.height) * 2.1
-          let videoNode = SCNNode()
-          videoNode.geometry = plane
-          let videoMaterial = self.createMaterial(videoUrl: videoVideoUrl, alpha: 0.7,angle: 0.0)
-          videoNode.geometry?.firstMaterial = videoMaterial
-          node.addChildNode(videoNode as SCNNode)
-          
-          let rockNode = MappingViewController.collada2SCNNode(filepath: "art.scnassets/Mossy Rock - stump.scn")
-          let rockMaterial = self.createMaterial(videoUrl: rockVideoUrl, alpha: 1.0,angle: 90.0)
-          rockNode.childNodes[0].geometry?.firstMaterial = rockMaterial
-          node.addChildNode(rockNode as SCNNode)
-          
-        }
-        //ライト
-        let lightNode = SCNNode()
-         lightNode.light = SCNLight()
-         lightNode.light?.type = .ambient
-         lightNode.position = SCNVector3(x:0, y:10, z:1)
-         node.addChildNode(lightNode as SCNNode)
-        
-        
-        node.simdTransform = imageAnchor.transform
-        node.eulerAngles.x = 0
-        self.playSound(name: "sound")
-      }
-    }
-  }
-  
-  
-  // オブジェクト用の動画 materialを生成
-  func createMaterial(videoUrl: URL,alpha: Float,angle: Float) -> SCNMaterial {
-    // AVPlayerを生成する
-    let avPlayer = AVPlayer(url: videoUrl)
-    // SKSceneを生成する
-    let skScene = SKScene(size: CGSize(width: 1000, height: 1000))
-    // AVPlayerからSKVideoNodeの生成する
-    let skNode = SKVideoNode(avPlayer: avPlayer)
-    // シーンと同じサイズとし、中央に配置する
-    skNode.position = CGPoint(x: skScene.size.width / 2.0, y: skScene.size.height / 2.0)
-    skNode.size = skScene.size
-    skNode.yScale = -1.0 // 座標系を上下逆にする
-    skNode.zRotation = CGFloat(angle)
-    skNode.play() // 再生開始
-    // SKSceneに、SKVideoNodeを追加する
-    skScene.addChild(skNode)
-    
-    let material = SCNMaterial()
-    material.diffuse.contents = skScene
-    material.transparency = CGFloat(alpha)
-    return material
-  }
-  
-  // collada2SCNNode
-  // scnファイルをnode化
-  public class func collada2SCNNode(filepath:String) -> SCNNode {
-    let node = SCNNode()
-    let scene = SCNScene(named: filepath)
-    let nodeArray = scene!.rootNode.childNodes
-    
-    for childNode in nodeArray {
-      node.addChildNode(childNode as SCNNode)
-    }
-    return node
-  }
-  // サウンド再生メソッド
-  func playSound(name: String) {
-    guard let path = Bundle.main.path(forResource: name, ofType: "mp3") else {
-      print("音源ファイルが見つかりません")
-      return
-    }
-    
-    do {
-      // AVAudioPlayerのインスタンス化
-      audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
-      
-      // AVAudioPlayerのデリゲートをセット
-      audioPlayer.delegate = self
-      
-      // 音声の再生
-      audioPlayer.play()
-    } catch {
-    }
-  }
     
     // Camera
     private func getScreenShot() -> UIImage? {
@@ -227,13 +101,4 @@ class MappingViewController: UIViewController, ARSCNViewDelegate, AVAudioPlayerD
         
         return image
     }
-  
-  func session(_ session: ARSession, didFailWithError error: Error) {
-  }
-  
-  func sessionWasInterrupted(_ session: ARSession) {
-  }
-  
-  func sessionInterruptionEnded(_ session: ARSession) {
-  }
 }

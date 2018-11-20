1ffb7327557ad9bda13af03efceb21b5407e6dcd
Syamoti <shot.sauser@gmail.com>
2018-11-20 13:55:30 +0900

    PhotoviewController へコメントでのプロマの分岐追加

--- gazer/PhotoPreViewController.swift
+++ gazer/PhotoPreViewController.swift
@@ -9,15 +9,25 @@
 import UIKit
 
 class PhotoPreViewController: UIViewController {
-    
+    // 動物園
     @IBOutlet weak var zooSaveButton: UIButton!
     @IBOutlet weak var zooPreviewImage: UIImageView!
+    
+    // 水族館
     @IBOutlet weak var aquaPreviewImage: UIImageView!
     @IBOutlet weak var aquaSaveButton: UIButton!
+    
+    //星
     @IBOutlet weak var starPreviewImage: UIImageView!
     @IBOutlet weak var starSaveButton: UIButton!
     
+    // プロマ
+/*
+    @IBOutlet weak var mappingPreviewImage: UIImageView!
+    @IBOutlet weak var mappingSaveButton: UIButton!
+*/
     
+    // 動物園
     @IBAction func zooSaveButton(_ sender: UIButton) {
         UIImageWriteToSavedPhotosAlbum(screenImage!, nil, nil, nil)
         self.dismiss(animated: true, completion: nil)
@@ -27,6 +37,7 @@ class PhotoPreViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
+    // 水族館
     @IBAction func aquaSaveButton(_ sender: UIButton) {
         UIImageWriteToSavedPhotosAlbum(screenImage!, nil, nil, nil)
         self.dismiss(animated: true, completion: nil)
@@ -35,6 +46,7 @@ class PhotoPreViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
+    // 星
     @IBAction func starSaveButton(_ sender: UIButton) {
         UIImageWriteToSavedPhotosAlbum(screenImage!, nil, nil, nil)
         self.dismiss(animated: true, completion: nil)
@@ -43,6 +55,16 @@ class PhotoPreViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
+    //プロマ
+/*
+     @IBAction func mappingSaveButton(_ sender: UIButton) {
+     UIImageWriteToSavedPhotosAlbum(screenImage!, nil, nil, nil)
+     self.dismiss(animated: true, completion: nil)
+     }
+     @IBAction func mappingCancelButton(_ sender: UIButton) {
+     self.dismiss(animated: true, completion: nil)
+     }
+*/
     var screenImage:UIImage? = nil
     var addImage = 0
     
@@ -54,18 +76,22 @@ class PhotoPreViewController: UIViewController {
         switch addImage {
         case 1:// 動物園
             zooPreviewImage.image = screenImage
-            zooSaveButton.layer.cornerRadius = 20.0 // 角丸のサイズ
+            zooSaveButton.layer.cornerRadius = 20.0 // 保存ボタン角丸
             
         case 2:// 水族館
             aquaPreviewImage.image = screenImage
-            aquaSaveButton.layer.cornerRadius = 20.0 // 角丸のサイズ
+            aquaSaveButton.layer.cornerRadius = 20.0 // 保存ボタ角丸
             
         case 3:// 星
             starPreviewImage.image = screenImage
-            starSaveButton.layer.cornerRadius = 20.0 // 角丸のサイズ
-
+            starSaveButton.layer.cornerRadius = 20.0 // 保存ボタン角丸角丸
+/*
+        case 4:// プロマ
+             mappingPreviewImage.image = screenImage
+             mappingSaveButton.layer.cornerRadius = 20.0 // 保存ボタン角丸
+*/
         default:// エラー
-            print("Error")
+            print("PhotoPreviewControllerError")
         }
     }
     
